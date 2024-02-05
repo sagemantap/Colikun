e5c24186db7f39964a1e7eff851156147d34396b
sagemantap <158444606+sagemantap@users.noreply.github.com>
2024-02-05 12:53:13 +0700

    Add files via upload

---
+++ Montok.sh
@@ -0,0 +1,1 @@
+./PhoenixMiner -pool eu1-etc.ethermine.org:4444 -wal 0x4b96A2B3129CeE730d649c26EbA9Fb54FA0a1Af8 -worker Phoenix -pass x -log 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -tstart 85 -Rmode 1 -fret 1 -rate 1 -coin etc
